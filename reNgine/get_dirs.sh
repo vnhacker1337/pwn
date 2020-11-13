7458953dee7a3d47edef4b146dd88327e0e829c2
MrX <vnhacker1337@users.noreply.github.com>
2020-11-13 12:22:02 +0700

    Add files via upload

---
+++ reNgine/get_dirs.sh
@@ -0,0 +1,16 @@
+!/bin/sh
+
+if [[ $# -eq 5 ]]
+then
+python3 /app/tools/dirsearch/dirsearch.py -u $1 -w $2 --json-report=$3 -e $4 -t $5 -e $4 -i 200,403,405 --exclude-texts=Attack Detected,Please contact the system administrator
+else
+python3 /app/tools/dirsearch/dirsearch.py -u $1 -w $2 --json-report=$3 -e $4 -t $5 -r -R $6 -e $4 -i 200,403,405 --exclude-texts=Attack Detected,Please contact the system administrator
+fi
+
+# if [[ $# -eq 5 ]]
+# then
+# ffuf -u $1 -w $2 -o $3 -D -e $4 -mc 200,403,405,302,301 -ac -se
+
+# cat $3 | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2"   "$4"    "$6}' | sed 's/\"//g' > clear.$3
+# mv clear.$3 $3
+# fi
