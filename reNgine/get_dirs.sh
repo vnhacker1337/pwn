f5c755f99e2d68a523eb958c812392e9be143c97
MrX <vnhacker1337@users.noreply.github.com>
2020-11-13 12:50:27 +0700

    Add files via upload

--- reNgine/get_dirs.sh
+++ reNgine/get_dirs.sh
@@ -2,9 +2,9 @@
 
 if [[ $# -eq 5 ]]
 then
-python3 /app/tools/dirsearch/dirsearch.py -u $1 -w $2 --json-report=$3 -e $4 -t $5 -e $4 -i 200,403,405 --exclude-texts=Attack Detected,Please contact the system administrator
+timeout 500 python3 /app/tools/dirsearch/dirsearch.py -u $1 -w $2 --json-report=$3 -e $4 -t $5 -e $4 -i 200,403,405 --exclude-texts=Attack Detected,Please contact the system administrator
 else
-python3 /app/tools/dirsearch/dirsearch.py -u $1 -w $2 --json-report=$3 -e $4 -t $5 -r -R $6 -e $4 -i 200,403,405 --exclude-texts=Attack Detected,Please contact the system administrator
+timeout 500 python3 /app/tools/dirsearch/dirsearch.py -u $1 -w $2 --json-report=$3 -e $4 -t $5 -r -R $6 -e $4 -i 200,403,405 --exclude-texts=Attack Detected,Please contact the system administrator
 fi
 
 # if [[ $# -eq 5 ]]
